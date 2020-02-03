f7455b08b79ed718f7f99cca90f1a6d9c3e9a721
Paulavancamp <paula.van.camp@gmail.com>
2020-02-02 19:44:31 -0600

    added test message

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
   /* Open Menu */
   $( ".open-close-btn" ).on('click touchstart', function(e) {
+      console.log("test1");
     // prevent default anchor click 
     e.preventDefault();
     $(".overlay").toggleClass("overlay-open");
