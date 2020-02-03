030792c15605622ab575262fda0694d99846ace8
Paulavancamp <paula.van.camp@gmail.com>
2020-02-02 19:18:09 -0600

    testing updated menu

--- menu.js
+++ menu.js
@@ -1,11 +1,7 @@
-(function($){
-  
-    var btn = $( 'button' );
-    var menu = $( 'nav' );
-    
-    btn.click(function(){
-      $( this ).toggleClass( 'on' );
-      menu.toggleClass( 'on' );
-    });
-    
-  })(jQuery);
+  /* Open Menu */
+  $( ".open-close-btn" ).on('click touchstart', function(e) {
+    // prevent default anchor click 
+    e.preventDefault();
+    $(".overlay").toggleClass("overlay-open");
+    $("#hamburger-icon").toggleClass("hamburger-open");
+});
