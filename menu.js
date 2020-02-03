bcbb42026d77135720d0325eec931c6aed74f42f
Paulavancamp <paula.van.camp@gmail.com>
2020-02-02 20:24:13 -0600

    removed js and animated with css

--- menu.js
+++ menu.js
@@ -1,14 +1,12 @@
   /* Open Menu */
-  var menuicon = document.getElementById("hamburger-icon");
-  var overlay = document.getElementById("overlay");
   console.log("loading page");
 
-  var toggleMenu = function(){
+  function toggleMenu(){
     console.log("test1");
     // prevent default anchor click 
     e.preventDefault();
+    var menuicon = document.getElementById("hamburger-icon");
+    var overlay = document.getElementById("overlay");
     overlay.toggleClass("overlay-open");
     menuicon.toggleClass("hamburger-open");
-}
-
-menuicon.addEventListener("click", toggleMenu);  
+} 
