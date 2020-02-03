d353fce1a530c6b1e7b57e85ba808dd7f8c22527
Paulavancamp <paula.van.camp@gmail.com>
2020-02-02 19:51:25 -0600

    translated jquery to js

--- menu.js
+++ menu.js
@@ -1,8 +1,13 @@
   /* Open Menu */
-  $( ".open-close-btn" ).on('click touchstart', function(e) {
-      console.log("test1");
+  var menuicon = document.getElementById("hamburger-icon");
+  var overlay = document.getElementById("overlay");
+
+  var toggleMenu = function(){
+    console.log("test1");
     // prevent default anchor click 
     e.preventDefault();
-    $(".overlay").toggleClass("overlay-open");
-    $("#hamburger-icon").toggleClass("hamburger-open");
-});
+    overlay.toggleClass("overlay-open");
+    menuicon.toggleClass("hamburger-open");
+}
+
+menuicon.addEventListener(toggleMenu, "click");  
